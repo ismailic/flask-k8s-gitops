75bafd24d2ee5b0586221e5b75553576b148fd94
ismailic <ismail.sehnoun00@gmail.com>
2026-04-17 20:33:25 +0000

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps 🚀 please welcome the new engineer AZIZ AID HAMOU"
+    return "Hello DevOps Team 🚀 please welcome the New engineers  Yassine Jaml abou dalal, Marouan Jaram 3achiq El gharam, AZIZ AID HAMOU sadiq Imade abou Maed"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
